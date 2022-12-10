730c8bc344604339dfd7d4f92ac057908fceaed1
leke-ife <ifeoluwaadeleke1@gmail.com>
2022-12-09 16:49:43 -0800

    print%n

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all strings 0 and 9
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; nun < 10; num++)
+		printf("%d", num);
+
+	prinff("\n");
+
+	return (0);
+}
