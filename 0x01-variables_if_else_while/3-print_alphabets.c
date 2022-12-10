8ed868f739e53595e0073c791136126d111896f9
leke-ife <ifeoluwaadeleke1@gmail.com>
2022-12-09 16:16:27 -0800

    print A

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h.>
+
+/**
+ * main - prnin in lower and higer case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A' <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
