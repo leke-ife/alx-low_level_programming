fa0ac354d7d22af20ad5545267537637d0a66566
leke-ife <ifeoluwaadeleke1@gmail.com>
2022-12-13 13:27:45 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute vluse of an intger
+ * @t: The integer to be computed
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
