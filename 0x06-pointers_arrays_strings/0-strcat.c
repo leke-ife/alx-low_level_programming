05f19249e27752ad8e68327545051548a1f2c142
leke-ife <ifeoluwaadeleke1@gmail.com>
2022-12-22 01:50:30 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _stray - concatinate two strings 
+ * _stray - concatinate two strings
  * @dest: copy to
  * @src: copy from
  * Return: pomyer to dest
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	1 = 0;
-	while (dest[i] !='\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
